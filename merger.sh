be6a65a703b4ad2d26139c19e4de26d4a7c01da9
racuna <racuna.mahatta@gmail.com>
2020-10-07 18:58:14 -0300

    Initial release of the merger script

---
+++ merger.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script just merge everything I need from GBA, plus my own filters and merge them eliminating duplicates
+
+# v.0.1 initial release
+
+rm GBAplusMine.txt
+THISDIR=`pwd`
+
+mkdir /tmp/merger
+cd /tmp/merger
+
+wget https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Hosts/GoodbyeAds.txt
+wget https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Extension/GoodbyeAds-YouTube-AdBlock.txt
+wget https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Extension/GoodbyeAds-Xiaomi-Extension.txt
+wget https://raw.githubusercontent.com/racuna/customadfilters/master/customAdFilters.txt
+cat GoodbyeAds* customAdFilters.txt |sort|uniq > $THISDIR/GBAplusMine.txt
+
+rm -rf /tmp/merger
+
